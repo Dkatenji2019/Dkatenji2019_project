c9b39fd96c1ceed237358863c57491b5f89279be
Kosihikari <m.teru85@gmail.com>
2019-09-29 16:28:01 +0900

    ignoreしました

---
+++ project/mikke2/Assets/chara.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chara : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed;
+    public float inputHorizontal;
+    public float inputVertical;
+
+    public Rigidbody rb;
+
+    private void movePlayer()
+    {
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        inputVertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 Direct = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+
+        Vector3 Forward = Direct * inputVertical + Camera.main.transform.right * inputHorizontal;
+
+        rb.velocity = Forward * speed + new Vector3(0, rb.velocity.y, 0);
+
+    }
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        speed = 8.0f;
+        transform.position = new Vector3(0, 10, 0);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        movePlayer();
+
+    }
+}
---
+++ project/mikke2/Assets/move.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private const int ROTATE_BUTTON = 0;
+    private const float ANGLE_LIMIT_UP = 60f;
+    private const float ANGLE_LIMIT_DOWN = -60f;
+
+    public float rotate_speed = 7.0f;
+
+    public GameObject player;
+
+    private void rotateCameraAngle()
+    {
+        Vector3 angle = new Vector3(
+        Input.GetAxis("Mouse X") * rotate_speed,
+        Input.GetAxis("Mouse Y") * -1 * rotate_speed,
+        0
+    );
+
+        transform.eulerAngles += new Vector3(angle.y, angle.x);
+    }
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position;
+
+        if (Input.GetMouseButton(ROTATE_BUTTON))
+        {
+            rotateCameraAngle();
+        }
+
+        float angle_x = 180f <= transform.eulerAngles.x ? transform.eulerAngles.x - 360 : transform.eulerAngles.x;
+        transform.eulerAngles = new Vector3(
+            Mathf.Clamp(angle_x, ANGLE_LIMIT_DOWN, ANGLE_LIMIT_UP),
+            transform.eulerAngles.y,
+            transform.eulerAngles.z
+        );
+    }
+}
