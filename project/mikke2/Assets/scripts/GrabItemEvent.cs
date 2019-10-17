3a3e8422903d5baf3071cf889265b8ac298796b7
katenjiPC-5 <56110428+katenjiPC-5@users.noreply.github.com>
2019-10-17 18:00:17 +0900

    [change]scorechangeの編集

--- project/mikke2/Assets/scripts/GrabItemEvent.cs
+++ project/mikke2/Assets/scripts/GrabItemEvent.cs
@@ -5,67 +5,10 @@ using UnityEngine.UI;
 
 public class GrabItemEvent : MonoBehaviour {
 
-    ////ちょっと借りるな。いつか書き直す…はず
-
-    ////circleGagescript内で取っている時間の処理をここでとることができれば、getcomponentの処理が減る
-    ////　[至急]　ifの入れ子を急いで解消しなければ
-    ////とても汚いので急いで書き直す
-
-    //public Camera camera_object; //カメラを取得
-    //private RaycastHit hit; //レイキャストが当たったものを取得する入れ物
-
-
-    //[SerializeField] private GameObject Odai;
-    //[SerializeField] private GameObject meter;//時間のメータをもっているUI(meter)
-
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    //マウスのポジションを取得してRayに代入
-    //    Ray ray = camera_object.ScreenPointToRay(Input.mousePosition);
-
-    //    //マウスのポジションからRayを投げて何かに当たったらhitに入れる
-    //    if (Physics.Raycast(ray, out hit))
-    //    {
-    //        if (hit.collider.gameObject.name != "Floor")
-    //        {
-    //            meter.GetComponent<CircleGage>().OnTrigger = true;
-
-    //            if (hit.collider.gameObject.GetComponent<ItemInformationOnPlaying>() != null)
-    //            {
-    //                Debug.Log("colliderhit:" + hit.collider.gameObject.GetComponent<ItemInformationOnPlaying>().MyItemName + ", OdaiQueue: " + Odai.GetComponent<OdaiList>()._itemsNameUiTexts.Peek());
-
-    //                if (meter.GetComponent<Image>().fillAmount >= 1 && hit.collider.gameObject.GetComponent<ItemInformationOnPlaying>().MyItemName == Odai.GetComponent<OdaiList>()._itemsNameUiTexts.Peek())
-    //                {
-    //                    Destroy(hit.collider.gameObject);
-    //                    Odai.GetComponent<OdaiList>()._itemsNameUiTexts.Dequeue();
-    //                    Odai.GetComponent<OdaiList>().AddTextToCanvas(Odai.GetComponent<OdaiList>()._itemsNameUiTexts.Peek(), Odai.GetComponent<OdaiList>().textMain);
-    //                }
-    //            }
-
-
-    //        }
-    //        else
-    //        {
-    //            meter.GetComponent<CircleGage>().OnTrigger = false;
-    //        }
-    //    }
-    //    else
-    //    {
-    //        meter.GetComponent<CircleGage>().OnTrigger = false;
-    //    }
-
-
-    //}
-
-
-
     public float GripTime { get; set; }
     readonly float DecideGripTime = 3.0f;
 
     public bool IsGripItem;
-
 
     public bool IsMatchedWithItem;
 
