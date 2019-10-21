8e5781a9eb28adba8218f1a29047f73066f624ea
katenjiPC-7 <56108041+katenjiPC-7@users.noreply.github.com>
2019-10-21 18:54:43 +0900

    Revert "[Change]..."
    This reverts commit ba2c9acdd234ecb406c8d7dcbe61d47278935e54.

--- project/mikke2/Assets/scripts/Mapping.cs
+++ project/mikke2/Assets/scripts/Mapping.cs
@@ -2,38 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mapping : MonoBehaviour
-{
+public class Mapping : MonoBehaviour {
 
-    // Use this for initialization
-
-    GameObject player;
-
-
-
-    // Use this for initialization
-
-    void Start()
-    {
-
-        player = GameObject.Find("[CameraRig]/Camera (eye)");
-
-    }
-
-
-
-    // Update is called once per frame
-
-    void Update()
-    {
-
-        // レーダをくるくる回す
-
-        transform.rotation = Quaternion.Euler(new Vector3(90f, player.transform.eulerAngles.y, 0));
-
-        // 自分をレーダの中心にする
-
-        transform.position = new Vector3(player.transform.position.x, 30f, player.transform.position.z);
-
-    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
