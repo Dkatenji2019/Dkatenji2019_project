8617939f56c5283c497b9eff08ab5ead3bd0ad2b
katenjiPC-7 <56108041+katenjiPC-7@users.noreply.github.com>
2019-10-24 16:48:48 +0900

    [add]folda構成の変更

--- project/mikke2/Assets/scripts/SpreadItemOnLab.cs
+++ project/mikke2/Assets/scripts/SpreadItemOnLab.cs
@@ -113,16 +113,16 @@ public class SpreadItemOnLab : MonoBehaviour
                 Vector2 pos1 = new Vector2(x, z);
                 Vector2 pos2 = new Vector2(0, 0);
 
-                while (Vector2.Distance(pos1, pos2) < 15 || Vector2.Distance(pos1, pos2) > 45)
-                {
-                    x = Random.Range(-area, area);
-                    z = Random.Range(-area, area);
+            while (Vector2.Distance(pos1, pos2) < 15 || Vector2.Distance(pos1, pos2) > 45)
+            {
+                x = Random.Range(-area, area);
+                z = Random.Range(-area, area);
 
-                    pos1 = new Vector2(x, z);
-                    pos2 = new Vector2(0, 0);
-                }
+                pos1 = new Vector2(x, z);
+                pos2 = new Vector2(0, 0);
+            }
 
-                float y = Random.Range(0, 30.0f);
+            float y = Random.Range(0, 30.0f);
                 Q.transform.position = new Vector3(x, y, z);
 
                 //instancedItem.AddComponent<ItemInformation>();
