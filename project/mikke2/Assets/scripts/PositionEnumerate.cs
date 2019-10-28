7797c6f869ee65195fceae04c78d06230e4617d2
katenjiPC-7 <56108041+katenjiPC-7@users.noreply.github.com>
2019-10-28 17:46:17 +0900

    [add]SpreadItemOnForrest,SpreadIemOnMiniatureの追加

--- project/mikke2/Assets/scripts/PositionEnumerate.cs
+++ project/mikke2/Assets/scripts/PositionEnumerate.cs
@@ -1,8 +1,14 @@
 ﻿public enum PositionEnumerate : int
 {
+    /*
     地面 = 0,
     テーブル = 1,
     棚 = 2,
     引き出し = 3,
+    */
+    Forrest_Ground = 10,
+    Lab_CenterTable = 21,
+    Lab_SideTable = 22,
+    Miniature_Ground = 30,
     この項目には指定しないこと_開発者用 = -1
 }
---
+++ project/mikke2/Assets/scripts/SpreadItemOnMiniature.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpreadItemOnMiniature : MonoBehaviour
+{
+    [SerializeField] private int area;
+    [SerializeField] private ItemRegistrator itemRegistrator;
+
+    // Use this for initialization
+    void Start()
+    {
+        putItemSpread();
+    }
+
+    private void putItemSpread()
+    {
+        foreach (var Q in itemRegistrator.ItemQ)
+        {
+            if (Q.gameObject.GetComponent<ItemInformation>().ItemRespawnPosition != 0)
+            {
+                continue;
+            }
+
+            /**/
+            float x = Random.Range(-area, area);
+            float z = Random.Range(-area, area);
+
+            Vector2 pos1 = new Vector2(x, z);
+            Vector2 pos2 = new Vector2(0, 0);
+
+            while (Vector2.Distance(pos1, pos2) < 15 || Vector2.Distance(pos1, pos2) > 45)
+            {
+                x = Random.Range(-area, area);
+                z = Random.Range(-area, area);
+
+                pos1 = new Vector2(x, z);
+                pos2 = new Vector2(0, 0);
+            }
+
+            float y = Random.Range(0, 30.0f);
+            Q.transform.position = new Vector3(x, y, z);
+
+            /**/
+        }
+
+    }
+}
+
+
